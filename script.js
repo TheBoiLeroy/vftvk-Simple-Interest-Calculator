d5c073d820b126281d874a1ac9d1f926ff082985
Ian <iansantos@my.smccd.edu>
2022-04-02 23:06:47 -0700

    Completed Lab

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //initializing all variables that will be used
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+
+  //if statement to check if the principal input is a valid input  
+    if(principal<=0)
+    {
+        alert("Enter a positive number")
+        
+        document.getElementById('principal').style.borderColor = "red";
+    }
+    else{
+        var interest = principal * years * rate /100;
+        var amount =parseInt(interest);
+        document.getElementById("result").innerHTML="\<br\> If you deposit <mark> "+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+
+
+    }
     
+   
 }
+//updates the percentage on the slider to display 
+function updateRate(event)
+{
+        var rateval = document.getElementById("rate").value
+        document.getElementById("rate_val").innerText=rateval + '%';
+} 
+   
+    
+
         
